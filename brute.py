433fb1a2c3e748a6f405e42cb9d7faa59d937a4a
UsefulBird <94715155+UsefulBird@users.noreply.github.com>
2021-12-12 13:27:29 +0100

    Add files via upload
    This is a brute force demonstration

---
+++ brute.py
@@ -0,0 +1,19 @@
+import random
+import pyautogui
+import string
+
+chars = string.printable
+chars_list = list(chars)
+
+password = pyautogui.password("Enter a password: ")
+
+guess_password = ""
+
+while(guess_password != password):
+    guess_password = random.choices(chars_list, k=len(password))
+
+    print("<==" + str(guess_password) + "==>")
+
+    if(guess_password == list(password)):
+        print("Your password is : "+ "".join(guess_password))
+        break
